e61ff0f5712a566da609b39be91bc9a15835f3dd
Abhimanyu Kanaujia <iabhimanyu.kanaujia@gmail.com>
2024-03-16 16:11:26 +0530

    Initial commit

---
+++ frontend/SkillSync/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-slate-500">hey</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
